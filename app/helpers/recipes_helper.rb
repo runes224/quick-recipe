a4d13ba23e6fa7bf7ddda1c07870144af40bec1d
runes224 <hitakarento@gmail.com>
2019-06-21 04:34:57 +0900

    wip

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,8 +1,2 @@
 module RecipesHelper
-  def show_ingredient_name
-    # IngredientRelation.find_by(id: value).display_ingredient_name
-    @recipe.ingredient_relations.each do |r|
-      r.weight
-    end
-  end
 end
