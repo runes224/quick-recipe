5fafd0088ce3774059c232db36b8ea19913aa26e
runes224 <hitakarento@gmail.com>
2019-07-15 15:41:01 +0900

    wip

--- Gemfile
+++ Gemfile
@@ -5,7 +5,8 @@ ruby '2.5.5'
 
 gem 'public_suffix', '~> 3.1'
 gem 'mini_racer'
-gem 'device'
+gem 'slim'
+gem 'html2slim'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
@@ -63,6 +64,7 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+    #gem 'simplecov', require: false
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   get  '/help',     to: 'static_pages#help'
   get  '/about',    to: 'static_pages#about'
   get  '/contact',  to: 'static_pages#contact'
-  get  '/signup',   to: 'users#new'
   get  '/login',    to: 'sessions#new'
   post '/login',    to: 'sessions#create'
   delete  '/logout',   to: 'sessions#destroy'
-  resources :sessions, only: [new]
-  resources :users, [new create show edit update]
-  resources :ingredients
-  resources :recipes
-  resources :ingredient_relations
+  get '/signup', to: 'users#new'
+  resources :users, only: [:create, :show, :destroy]
+  resources :recipes, only: [:new, :create, :show, :update, :destroy]
+  resources :ingredients, only: [:create, :update, :destroy]
+  resources :ingredient_relations, only: [:create, :destroy]
+  resources :myrecipes, only: [:create, :destroy]
 end
