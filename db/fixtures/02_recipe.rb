a9003f5a430e08eeef10a8e3f33801770e515ea1
runes224 <hitakarento@gmail.com>
2019-10-11 00:02:26 +0900

    初期データの整備

--- db/fixtures/02_recipe.rb
+++ db/fixtures/02_recipe.rb
@@ -1,8 +1,8 @@
 100.times do |i|
   Recipe.seed do |s|
     s.id = i
-    s.name = "キーマカレー #{i + 1}"
-    s.description = "健康的なキーマカレーです。 #{i + 1}"
+    s.name = "キーマカレー。 #{i + 1}"
+    s.description = "健康的なキーマカレーです #{i + 1}"
     s.user = User.find_by(id: i)
     s.image = File.open("#{Rails.root}/db/curry.jpg")
   end
@@ -11,7 +11,7 @@ end
 Recipe.seed do |s|
   s.id = 200
   s.name = "鯖水煮カレー"
-  s.description = "鯖を食べてDHA・EPAを摂取しましょう。カレー味なので魚臭さがなく、お子さんにもおすすめです！!"
+  s.description = "鯖を食べてDHA・EPAを摂取しましょう。カレー味なので魚臭さがなく、お子さんにもおすすめです！!!！"
   s.user = User.find_by(id: 1)
   s.image = File.open("#{Rails.root}/db/saba_curry.jpg")
 end
