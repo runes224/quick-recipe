98eb18bb70d5ad49d458d4962cbb40b076d8f75a
runes224 <hitakarento@gmail.com>
2019-11-05 23:29:54 +0900

    初期データの整備

--- db/fixtures/02_recipe.rb
+++ db/fixtures/02_recipe.rb
@@ -11,7 +11,7 @@ end
 Recipe.seed do |s|
   s.id = 200
   s.name = "鯖水煮カレー!"
-  s.description = "鯖を食べてDHA・EPAを摂取しましょう。カレー味なので魚臭さがなく、お子さんにもおすすめです！"
+  s.description = "さばを食べてDHA・EPAを摂取しましょう。カレー味なので魚臭さがなく、お子さんにもおすすめです！"
   s.user = User.find_by(id: 1)
   s.image = File.open("#{Rails.root}/db/saba_curry.jpg")
 end
